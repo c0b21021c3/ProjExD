3f0f71fc0e48635a1cea5508494c56f68ac5d97b
umemiya <c0b21021c3@edu.teu.ac.jp>
2022-09-27 18:18:56 +0900

    すっきりさせてみました #1

--- ex01/alphabet.py
+++ ex01/alphabet.py
@@ -22,6 +22,29 @@ def rtxt(txt):
     random.shuffle(t)
     return (t, r)
 
+def m1():#欠損文字列数を問う問題１を出題し、正負を判断するflag1を返す
+    global ln
+    ans1 = int(input("欠損文字はいくつあるでしょうか？: "))
+    if ans1 == ln:
+        print("正解です")
+        flag1 = True
+    else:
+        print("不正解です")
+        flag1 = False
+    return flag1
+
+def m2(rmw):#欠損文字列を問う問題２を出題し、正負を判断するflag2を返す
+    ans2 = []
+    for i in range(1,ln+1):#欠損文字列数の数だけ問う
+        ans2.append(input(f"{i}つ目の文字を入力してください: "))
+    if (ans2 == rmw) or (ans2[::-1] == rmw):
+        print("正解です")
+        flag2 = True
+    else:
+        print("不正解です")
+        flag2 = False
+    return flag2
+
 start = time.time()
 times = 1
 while times <= f:
@@ -30,19 +53,9 @@ while times <= f:
     rmtxt , rmw = rtxt(txt)
     ts = [" ".join(i) for i in [txt,rmtxt,rmw]]
     print(f"対象文字:\n{ts[0]}\n表示文字:\n{ts[1]}\n")
-    ans1 = int(input("欠損文字はいくつあるでしょうか？: "))
-    if ans1 == ln:
-        print("正解です")
-        ans2 = []
-        for i in range(1,ln+1):
-            ans2.append(input(f"{i}つ目の文字を入力してください: "))
-        if (ans2 == rmw) or (ans2[::-1] == rmw):
-            print("正解です")
+    if m1():
+        if m2(rmw):
             times = f+1
-            break
-        else:print("不正解です")
-
-    else: print("不正解です")
 
 finish = time.time() - start
 print(f"経過時間: {round(finish)}秒")
