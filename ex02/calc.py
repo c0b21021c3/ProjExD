0e07489c5f06c0456b15af76abd455ee15ada3b4
umemiya <c0b21021c3@edu.teu.ac.jp>
2022-10-04 16:00:21 +0900

    課題7

--- ex02/calc.py
+++ ex02/calc.py
@@ -5,10 +5,17 @@ from tkinter import messagebox as tkm
 def button_click(event):
     btn = event.widget
     txt = btn["text"]
+    if txt == "=":
+        a = entry.get()
+        entry.delete(0,tk.END)
+        ans = eval(a)
+        txt = ans
+        
+
     entry.insert(tk.END, txt)
 
 root = tk.Tk()
-root.geometry("300x500")
+root.geometry("300x600")
 
 ##数字盤
 x = 0#column
@@ -26,6 +33,11 @@ for i, num in enumerate(range(9,-1,-1),1):
 pls = tk.Button(root, text=f"+",font=("",30),width=3,height=2)
 pls.bind("<1>",button_click)
 pls.grid(row=4,column=1)
+
+##=ボタン
+pls = tk.Button(root, text=f"=",font=("",30),width=4,height=2)
+pls.bind("<1>",button_click)
+pls.grid(row=5,column=2)
 
 ##入力欄
 entry = tk.Entry(root, width=10, font=("",40),justify="right")
