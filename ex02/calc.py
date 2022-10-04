a56314e1c4c77cbd266cc8982f9a5d78dab1043c
umemiya <c0b21021c3@edu.teu.ac.jp>
2022-10-04 16:51:31 +0900

    機能追加後

--- ex02/calc.py
+++ ex02/calc.py
@@ -1,4 +1,4 @@
-from re import X
+import random
 import tkinter as tk
 from tkinter import messagebox as tkm
 
@@ -9,13 +9,16 @@ def button_click(event):
         a = entry.get()
         entry.delete(0,tk.END)
         ans = eval(a)
-        txt = ans
-        
-
-    entry.insert(tk.END, txt)
+        entry.insert(tk.END, ans)
+    elif "AC" in txt:
+        entry.delete(0,tk.END)
+    elif "Rnd"in txt:
+        Rnd = random.randint(1,100)
+        entry.insert(tk.END, Rnd)
+    else:entry.insert(tk.END, txt)
 
 root = tk.Tk()
-root.geometry("300x600")
+root.geometry("400x600")
 
 ##数字盤
 x = 0#column
@@ -29,18 +32,50 @@ for i, num in enumerate(range(9,-1,-1),1):
         y += 1
         x = 0
 
-##＋ボタン
-pls = tk.Button(root, text=f"+",font=("",30),width=3,height=2)
-pls.bind("<1>",button_click)
-pls.grid(row=4,column=1)
+###数字盤追加
+#0.
+btn= tk.Button(root, text="0.",font=("",30),width=4,height=2)
+btn.bind("<1>",button_click)
+btn.grid(row=4,column=1)
+#00
+btn= tk.Button(root, text="00",font=("",30),width=4,height=2)
+btn.bind("<1>",button_click)
+btn.grid(row=4,column=2)
 
-##=ボタン
-pls = tk.Button(root, text=f"=",font=("",30),width=4,height=2)
+##＋ボタン
+pls = tk.Button(root, text=f"+",font=("",30),width=4,height=1,bg="Orange")
+pls.bind("<1>",button_click)
+pls.grid(row=5,column=0)
+##-ボタン
+pls = tk.Button(root, text=f"-",font=("",30),width=4,height=1,bg="Orange")
+pls.bind("<1>",button_click)
+pls.grid(row=5,column=1)
+##*ボタン
+pls = tk.Button(root, text=f"*",font=("",30),width=4,height=1,bg="Orange")
 pls.bind("<1>",button_click)
 pls.grid(row=5,column=2)
+##/ボタン
+pls = tk.Button(root, text=f"/",font=("",30),width=3,height=1,bg="Orange")
+pls.bind("<1>",button_click)
+pls.grid(row=5,column=3)
+##=ボタン
+pls = tk.Button(root, text=f"=",font=("",30),width=3,height=2,bg="Orange")
+pls.bind("<1>",button_click)
+pls.grid(row=3,column=3)
+#ACボタン
+pls = tk.Button(root, text=f"AC",font=("",30),width=3,height=2,bg="Orange")
+pls.bind("<1>",button_click)
+pls.grid(row=1,column=3)
+#乱数randomボタン
+pls = tk.Button(root, text=f"Rnd",font=("",30),width=3,height=2,bg="Orange")
+pls.bind("<1>",button_click)
+pls.grid(row=2,column=3)
+#%ボタン
+pls = tk.Button(root, text=f"%",font=("",30),width=3,height=2,bg="Orange")
+pls.bind("<1>",button_click)
+pls.grid(row=4,column=3)
 
 ##入力欄
-entry = tk.Entry(root, width=10, font=("",40),justify="right")
-entry.grid(row=0,column=0,columnspan=3)
-    
+entry = tk.Entry(root, width=14, font=("",40),justify="right")
+entry.grid(row=0,column=0,columnspan=5)
 root.mainloop()
