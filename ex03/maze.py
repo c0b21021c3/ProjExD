0816e90436251f98ee0bc9edda86425820bc3f40
umemiya <c0b21021c3@edu.teu.ac.jp>
2022-10-18 14:19:39 +0900

    練習3

--- ex03/maze.py
+++ ex03/maze.py
@@ -12,5 +12,8 @@ root.title("迷えるこうかとん")
 canv = tk.Canvas(width=1500,height=900,bg="Black")
 canv.pack()
 
+chkn = tk.PhotoImage(file="./ex03/fig/1.png")
+cx, cy = 300, 400
+canv.create_image(cx, cy, image=chkn, tag = "KoKaTon")
 
 root.mainloop()
