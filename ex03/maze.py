fbcee9fe6ade612604733fdd93279484c3174708
umemiya <c0b21021c3@edu.teu.ac.jp>
2022-10-18 14:41:20 +0900

    練習7

--- ex03/maze.py
+++ ex03/maze.py
@@ -8,6 +8,19 @@ def key_up(event):
     global key
     key = ""
 
+def main_proc():
+    global cx, cy, key
+    
+    if key=="Up":#キーによる移動条件分岐
+        cy -= 20
+    if key=="Down":
+        cy += 20
+    if key=="Left":
+        cx -= 20
+    if key=="Right":
+        cx += 20
+    canv.coords("k",cx, cy)
+    root.after(100,main_proc)
 
 
 #------------------def/main
@@ -20,15 +33,15 @@ canv.pack()
 
 ##こーかとん採取
 chkn = tk.PhotoImage(file="./ex03/fig/1.png")
-cx, cy = 300, 400
-canv.create_image(cx, cy, image=chkn, tag = "KoKaTon")#はっつける
+cx, cy = 300, 400#こーかとｎ座標
+canv.create_image(cx, cy, image=chkn, tag = "k")#はっつける
 
 ###キー関連
 key = ""#現在押されているキーを表す変数
 root.bind("<KeyPress>", key_down)
-root.bind("<KeyRelese>", key_up)
+root.bind("<KeyRelease>", key_up)
 
-
-
+####こーかとん動きます
+main_proc()
 
 root.mainloop()
