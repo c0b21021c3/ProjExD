69f4aead2d549db8b1afe049315d4d1872016db4
umemiya <c0b21021c3@edu.teu.ac.jp>
2022-10-18 14:25:46 +0900

    練習5修正

--- ex03/maze.py
+++ ex03/maze.py
@@ -3,7 +3,7 @@ import tkinter as tk
 def key_down(event):
     global key
     key = event.keysym
-    
+
 
 
 
@@ -11,14 +11,16 @@ def key_down(event):
 #------------------def/main
 root = tk.Tk()
 root.title("迷えるこうかとん")
-
+#キャンバス
 canv = tk.Canvas(width=1500,height=900,bg="Black")
 canv.pack()
-
+##こーかとん採取
 chkn = tk.PhotoImage(file="./ex03/fig/1.png")
 cx, cy = 300, 400
-canv.create_image(cx, cy, image=chkn, tag = "KoKaTon")
-
+canv.create_image(cx, cy, image=chkn, tag = "KoKaTon")#はっつける
+#キー関連
 key = ""#現在押されているキーを表す変数
+root.bind("<KeyPress>", key_down)
+
 
 root.mainloop()
